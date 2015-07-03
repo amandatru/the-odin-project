464244427242d46bea18e4e9161cf183b2b0789b
Amanda Truong <amt140130@utdallas.edu>
2015-07-03 01:40:33 -0500

    solved Problem 2 of Proj Euler and added comments to Problem 1

--- project-euler/problem1.js
+++ project-euler/problem1.js
@@ -1,4 +1,9 @@
-var sumOfMult3 = function() {
+/* 
+ * This program calculates the sum of all multiples of 3 or 5 below 1000.
+ *
+*/
+
+var sumOfMult3 = function() { //function that calculates sum of all multiples of 3 below 1000
   var sum3 = 0;
     for (var i =0; i<1000; i++) {
         if (i % 3 === 0) {
@@ -8,7 +13,7 @@ var sumOfMult3 = function() {
 return sum3;
 }
 
-var sumOfMult5 = function() {
+var sumOfMult5 = function() { //function that calculates sum of all multiples of 5 below 1000
   var sum5 = 0;
     for (var i =0; i<1000; i++) {
         if (i % 5 === 0) {
@@ -19,7 +24,7 @@ return sum5;
 }
 
 
-var sumOfMult3and5 = function() {
+var sumOfMult3and5 = function() { //function that calulcates sum of all multiples of 3 AND 5 below 1000
   var sum35 = 0;
     for (var i =0; i<1000; i++) {
         if (i % 5 === 0 && i % 3 === 0) {
@@ -30,12 +35,13 @@ return sum35;
 }
 
 var sum3 = sumOfMult3();
-console.log(sum3);
+console.log("The sum of the multiples of 3 below 1000 is: " + sum3);
 var sum5 = sumOfMult5();
-console.log(sum5);
+console.log("The sum of the multiples of 5 below 1000 is: " + sum5);
 var sum35 = sumOfMult3and5();
-console.log(sum35);
-var finalSum = sum3 + sum5 - sum35;
-console.log(finalSum);
+console.log("The sum of the multiples of 3 and 5 below 1000 is: " + sum35);
+var finalSum = sum3 + sum5 - sum35; //The sum of the multiples of 3 and 5 subtracted by multiples that are 
+                                    //factors of both to get rid of duplicate numbers.
+console.log("The sum of all multiples of 3 OR 5 below 1000 is: " + finalSum);
 
 
---
+++ project-euler/problem2.js
@@ -0,0 +1,28 @@
+/* 
+ * Euler-Project Problem 2
+ * This program calculates the sum of the even numbers of the fibonacci sequence up to 4 million.
+ * 
+*/
+
+var num1 = 1; //first number in fibonacci seq
+var num2 = 2; //second number in fibonacci seq
+var evenNumSum = 2; //Sum of even numbers initialized to 2 as starting amt
+
+var fibonacci = function(num1, num2) { //fibonacci sequence function
+  next = num1 + num2; //next number in sequence calculated by previous 2 numbers
+  console.log(num1 + " " + num2 + " " + next);
+  if (next % 2 === 0) { //if number is even, add to total even number sum
+    evenNumSum += next;
+    console.log("Sum so far: " + evenNumSum);
+  }
+  while (next < 4000000) { //while loop that uses recursive function calling to calculate the sum of the even numbers 
+                           //until the range of 4 million is reached
+  fibonacci(num2, next);
+ } 
+  if (next > 4000000) { //when 4 million is reached, sum is returned and program breaks out of the recursive call
+    return evenNumSum;
+  }
+}
+
+var sum = fibonacci(num1, num2);
+console.log("Total sum: " + sum);
